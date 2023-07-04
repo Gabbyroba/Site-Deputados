7adbccce029dae408b48cd4a888bd68036d286cd
Gabbyroba <gabbyramosbr2@gmail.com>
2023-07-04 12:28:04 -0300

    Finalizando Arquivos

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
 
 # Configurar título da página
 st.set_page_config(page_title='Despesas Deputados Federais 2023')
@@ -24,18 +23,18 @@ def carregar_dados2():
     tabela2 = pd.read_csv('despesasdepfinalcopia.csv')
     return tabela2
 
+# Somar valores da coluna valorLiquido
+total_gastos = df['valorLiquido'].sum()
+
+# Plotar um cartão com a soma
+st.subheader('Valor Líquido')
+st.info(f"R${total_gastos:.2f}")
+
 with st.container():
     # Título e Separador
     st.subheader('Análise de dados DF - 2023')
     st.title('Gráficos')
     st.write('---')
-
-# Somar valores da coluna valorLiquido
-total_gastos = df['valorLiquido'].sum()
-
-# Plotar um cartão com a soma 
-st.subheader('Valor Líquido')
-st.info(f"R${total_gastos:.2f}")
 
 with st.container():
     st.subheader('Dashboard Deputados')
@@ -48,7 +47,7 @@ with st.container():
     st.components.v1.html(html_code, height=600)
 
 with st.container():
-   # Calcular a contagem de ocorrência de cada siglaUF
+    # Calcular a contagem de ocorrência de cada siglaUF
     counts = df['siglaUF'].value_counts()
 
     # Criar um DataFrame com as informações
@@ -61,17 +60,20 @@ with st.container():
         return f"{value:.2f}"
 
     # Criar o treemap
-    fig2 = go.Figure(go.Treemap(
-        labels=df_treemap['siglaUF'],
-        parents=[""] * len(df_treemap),
-        values=df_treemap['valorLiquido'],
-        text=df_treemap['valorLiquido'].apply(format_value),
-        textposition="middle center",
-        hovertemplate='<b>%{label}</b> <br> Valor: %{text}',
-    ))
-
-    # Configurar o título
-    fig2.update_layout(title='Gastos por UF')
+    fig2 = {
+        'data': [{
+            'type': 'treemap',
+            'labels': df_treemap['siglaUF'],
+            'parents': [""] * len(df_treemap),
+            'values': df_treemap['valorLiquido'],
+            'text': df_treemap['valorLiquido'].apply(format_value),
+            'textposition': "middle center",
+            'hovertemplate': '<b>%{label}</b> <br> Valor: %{text}'
+        }],
+        'layout': {
+            'title': 'Gastos por UF'
+        }
+    }
 
     # Exibir o gráfico no Streamlit
     st.plotly_chart(fig2, use_container_width=True)
@@ -84,15 +86,19 @@ with st.container():
     y = df_sorted['valorLiquido']
 
     # Criar o gráfico de barras
-    fig = go.Figure(data=go.Bar(x=x, y=y))
-
-    # Atualizar o layout
-    fig.update_layout(
-        title='Gastos por partido',
-        xaxis_title='Sigla do Partido',
-        yaxis_title='Valor das Despesas',
-        barmode='group'
-    )
+    fig = {
+        'data': [{
+            'type': 'bar',
+            'x': x,
+            'y': y
+        }],
+        'layout': {
+            'title': 'Gastos por partido',
+            'xaxis': {'title': 'Sigla do Partido'},
+            'yaxis': {'title': 'Valor das Despesas'},
+            'barmode': 'group'
+        }
+    }
 
     # Plotar o gráfico usando Plotly
     st.plotly_chart(fig, use_container_width=True)
@@ -103,22 +109,25 @@ with st.container():
     categories = df2['siglaPartido'].unique()
 
     # Criar gráfico de barras agrupadas
-    fig = go.Figure()
+    fig = {
+        'data': [],
+        'layout': {
+            'xaxis': {'title': 'Tipo de Despesa'},
+            'yaxis': {'title': 'Valor Despesas'},
+            'barmode': 'stack',
+            'title': 'Gastos por Tipo de Despesa',
+            'xaxis_tickangle': -45
+        }
+    }
 
     for partido in categories:
         partido_data = df2[df2['siglaPartido'] == partido]
-        fig.add_trace(go.Bar(x=x, y=partido_data['valorLiquido'], name=partido))
-
-    # Configurar layout
-    fig.update_layout(
-        xaxis_title='Tipo de Despesa',
-        yaxis_title='Valor Despesas',
-        barmode='stack',
-        title='Gastos por Tipo de Despesa'
-    )
-
-    # Rotacionar eixo x
-    fig.update_layout(xaxis_tickangle=-45)
+        fig['data'].append({
+            'type': 'bar',
+            'x': x,
+            'y': partido_data['valorLiquido'],
+            'name': partido
+        })
 
     # Plotar gráfico com plotly
     st.plotly_chart(fig, use_container_width=True)
